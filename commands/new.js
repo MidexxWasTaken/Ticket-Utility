a3b7e8737509d5a89d4ac49b8f87480c0d6c21a8
Midexx <79418198+MidexxWasTaken@users.noreply.github.com>
2024-03-03 17:06:52 -0500

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -1,7 +1,6 @@
-
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { file_check, file_write } = require('../handlers/functions');
-
+ 
 module.exports = {
     data: {
         name: 'new',
@@ -11,12 +10,12 @@ module.exports = {
     async execute(i) {
         await i.deferReply({ ephemeral: true });
         const stp = await file_check(`${i.client.sts.dir}/Setup/${i.guild.id}.json`);
-
+ 
         if (!stp) {
             await i.editReply('*Configuration not found.*');
             return;
         }
-
+ 
         const category = await i.guild.channels.fetch(stp.category);
         if (category.children.size >= 50) {
             await i.editReply({ content: '*The staff is overloaded with tickets, try again later.*' });
@@ -28,17 +27,17 @@ module.exports = {
                 { id: i.user.id, type: 'member', allow: ['ADD_REACTIONS', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'USE_EXTERNAL_EMOJIS'], deny: ['CREATE_INSTANT_INVITE', 'CREATE_PUBLIC_THREADS']},
                 { id: stp.role, type: 'role', allow: ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'], deny: ['SEND_MESSAGES'] }
             ];
-
+ 
             const ticket = await category.createChannel(name, { reason: 'Opened a Ticket', topic: `**USER:** ${i.user.tag} | **ID:** ${i.user.id}`, permissionOverwrites: perms });
             const uptade = { role: stp.role, category: stp.category, ticket: (stp.ticket + 1) };
             file_write(`${i.client.sts.dir}/Setup/${i.guild.id}.json`, uptade);
-            
-            const buttons = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('Button-claim').setEmoji('✋').setLabel('Claim').setStyle('SUCCESS'),
-                new MessageButton().setCustomId('Button-cancel').setEmoji('❌').setLabel('Cancel').setStyle('DANGER')
+ 
+            const buttons = new ButtonBuilder().addComponents(
+                new ButtonBuilder().setCustomId('Button-claim').setEmoji('✋').setLabel('Claim').setStyle(ButtonStyle.Primary),
+                new ButtonBuilder().setCustomId('Button-cancel').setEmoji('❌').setLabel('Cancel').setStyle(ButtonStyle.Primary)
             );
-            const emb = new MessageEmbed().setColor('#FB8C00').setFooter({ text: `Ticket created by ${i.user.tag}`, iconURL: i.user.displayAvatarURL({ dynamic: true }) }).setDescription('A staff member will get back to you soon!').setTitle(`:ticket: Ticket #${stp.ticket + 1}`);
-
+            const emb = new EmbedBuilder().setColor('#FB8C00').setFooter({ text: `Ticket created by ${i.user.tag}`, iconURL: i.user.displayAvatarURL({ dynamic: true }) }).setDescription('A staff member will get back to you soon!').setTitle(`:ticket: Ticket #${stp.ticket + 1}`);
+ 
             const msg = await ticket.send({ embeds: [emb], components: [buttons] });
             await i.editReply({ content: `Your ticket has been created at [${ticket.name}](${msg.url})` });
         }
